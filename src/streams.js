d0b8b2521eb2e575cedfe9d65221f5d08eafa981
Paul Walsh <twentytwentyk@live.com>
2018-12-03 10:49:52 -0500

    centaur

--- src/streams.js
+++ src/streams.js
@@ -1,11 +1,23 @@
 var fs = require('fs')
 var through = require('through2')
 
-fs.createReadStream(process.argv[2]) 
+process.stdin
 .pipe(through(toUpper))
 .pipe(process.stdout)
-//process.argv returns an array containing the command line arguments passed
 
 function toUpper (buf, enc, next) {
-  next(null, buf.toString().toUpperCase())
+    next(null, buf.toString().toUpperCase() + 
+    `\n ${100 + 100} \n
+    <=======]}======
+        --.   /|
+      _\"/_.'/
+    .'._._,.'
+    :/ \{}/
+    (L  /--',----._
+        |          \\
+      : /-\ .'-'\ / |
+        \\, ||    \|
+        \/ ||    ||
+    `
+  )
 }
