823dcd1ecba995b1db5ae76de9e86541ea7a1648
Paul Walsh <twentytwentyk@live.com>
2018-12-03 10:27:46 -0500

    uppercase stream

--- src/streams.js
+++ src/streams.js
@@ -1,5 +1,13 @@
 var fs = require('fs')
+var through = require('through2')
 
 fs.createReadStream(process.argv[2]) 
+.pipe(through(toupper()))
 .pipe(process.stdout)
 //process.argv returns an array containing the command line arguments passed
+
+function toupper () {
+  return through(function (buf, enc, next) {
+    next(null, buf.toString().toUpperCase())
+  })
+}
