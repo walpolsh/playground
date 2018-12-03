5c71663e514b7af940fafd64c3884cb402e02836
Paul Walsh <twentytwentyk@live.com>
2018-12-03 11:50:42 -0500

    express server with react https://medium.freecodecamp.org/how-to-make-create-react-app-work-with-a-node-backend-api-7c5c48acb1b0

---
+++ client/src/components/Playground.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+let text = 'sdfsdfsdfsdfsdfsd'
+
+const Playground = (props) => <div>
+  <p>{props.response}</p>
+</div>
+export default Playground
--- src/components/Playground.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-
-let text = 'sdfsdfsdfsdfsdfsd'
-
-const Playground = () => <div>
-  ssdfsdfsdgasdfgjlkj {text}
-</div>
-export default Playground
