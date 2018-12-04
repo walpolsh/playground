273810925b248d9918fba7685d9137577f1d2934
Paul Walsh <twentytwentyk@live.com>
2018-12-04 10:15:59 -0500

    concat streams

--- client/src/cat.js
+++ client/src/cat.js
@@ -1,5 +1,25 @@
 var concat = require('concat-stream')
+//Writable stream that concatenates all the data from a stream and calls a callback with the result. Use this when you want to collect all the data from a stream into a single buffer.
+var http = require('http')
+var qs = require('querystring')
+var through = require ('through2')
 
-process.stdin.pipe(concat(function (body) {
-  console.log(body.length)
-}))
+var server = http.createServer(function (req, res) {
+  req.pipe(counter())
+    .pipe(concat({ encoding: 'string' }, onbody)) 
+  function counter () {
+    var size = 0;
+    return through(function (buf, enc, next) {
+      size += buf.length
+      if (size > 20) next(null, null)
+      else next(null, buf)
+    })
+  }
+  function onbody (body) {
+    var params = qs.parse(body)
+    console.log(params)
+    res.end('ok\n')
+  }
+})
+
+server.listen(5000)
